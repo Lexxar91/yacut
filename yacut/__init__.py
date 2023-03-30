ea28165749787246c8d4dba43b860eb6be6fb596
Дмитрий Муратов <myratov91@yandex.ru>
2023-03-30 15:52:23 +0300

    raise exp v3

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -6,6 +6,6 @@ from settings import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db=db)
 
 from . import api_views, error_handlers, models, views
